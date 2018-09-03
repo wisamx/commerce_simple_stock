b14a8b6a7787371181daf59420caf31c28512de3
Wisam Mways <wisam-mo@hotmail.com>
2018-09-03 19:02:41 +0300

    merge NelsNose fixes and contributions

--- src/OrderProcessor/AvailabilityOrderProcessor.php
+++ src/OrderProcessor/AvailabilityOrderProcessor.php
@@ -11,7 +11,6 @@ use Drupal\commerce_store\Entity\StoreInterface;
 use Drupal\Core\Messenger\MessengerTrait;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 
-
 class AvailabilityOrderProcessor implements OrderProcessorInterface {
 
   use MessengerTrait;
@@ -22,36 +21,52 @@ class AvailabilityOrderProcessor implements OrderProcessorInterface {
       $purchased_entity = $order_item->getPurchasedEntity();
       if ($purchased_entity) {
         $order_quantity = floatval($order_item->getQuantity());
-		
-		if ($purchased_entity->hasField('field_stock') && $purchased_entity->field_stock->value != null) {
-			
-			$stock = (integer) $purchased_entity->field_stock->value;
-			
-			// Remove order item there is no quantity available.
-			if (empty($stock) || $stock <= 0) {
-			  $order->removeItem($order_item);
-			  $order_item->delete();
-			  $this->messenger()->addError(
-				$this->t('@purchasable is no longer available and has been removed from your order.', [
-				  '@purchasable' => $purchased_entity->label()
-				])
-			  );
-			}
-			// Adjust to max quantity available if over.
-			elseif ($stock < $order_quantity) {
-			  $this->messenger()->addError(
-				$this->t('@purchasable only has @quantity available. Your order has been updated.', [
-				  '@purchasable' => $purchased_entity->label(),
-				  '@quantity' => $stock,
-				])
-			  );
-			  $order_item->setQuantity($stock);
-			}
-			
-		}
-		
+
+        if ($purchased_entity->hasField('field_stock') && $purchased_entity->field_stock->value != NULL) {
+
+          $stock = (integer) $purchased_entity->field_stock->value;
+          $allow_negative = \Drupal::config('commerce_simple_stock.settings')->get('allow_backorder');
+
+          // Remove order item there is no quantity available.
+          if (empty($stock) || $stock <= 0) {
+            if (!$allow_negative) {
+              $order->removeItem($order_item);
+              $order_item->delete();
+              $this->messenger()->addError(
+                $this->t('@purchasable is no longer available and has been removed from your order.', [
+                  '@purchasable' => $purchased_entity->label()
+                ])
+              );
+            }
+            else {
+              $this->messenger()->addError(
+                $this->t('@purchasable may be backordered.', [
+                  '@purchasable' => $purchased_entity->label()
+                ])
+              );
+            }
+          }
+          // Adjust to max quantity available if over.
+          elseif ($stock < $order_quantity) {
+            if (!$allow_negative) {
+              $this->messenger()->addError(
+                $this->t('@purchasable only has @quantity available. Your order has been updated.', [
+                  '@purchasable' => $purchased_entity->label(),
+                  '@quantity' => $stock,
+                ])
+              );
+              $order_item->setQuantity($stock);
+            }
+            else {
+              $this->messenger()->addError(
+                $this->t('@purchasable may be partially backordered.', [
+                  '@purchasable' => $purchased_entity->label()
+                ])
+              );
+            }
+          }
+        }
       }
     }
   }
-
 }
